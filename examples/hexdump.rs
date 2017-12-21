87bbd9c8257f419b153e32cb1600845a0ff2f2b4
Marcel Hellwig <git@cookiesoft.de>
2017-12-21 13:17:14 +0100

    add simple hexdump example which reads a file into the buffer

---
+++ examples/hexdump.rs
@@ -0,0 +1,30 @@
+extern crate cursive;
+extern crate cursive_hexview;
+
+use std::env;
+use std::fs::File;
+use std::io::{self, Read};
+use std::path::Path;
+use cursive_hexview::{DisplayState, HexView};
+use cursive::Cursive;
+use cursive::views::{Dialog, DummyView, LinearLayout, TextView};
+
+fn read_file(path: &Path) -> Result<Vec<u8>, io::Error> {
+    let mut file = File::open(path)?;
+    let mut buf = Vec::new();
+    file.read_to_end(&mut buf)?;
+    Ok(buf)
+}
+
+fn main() {
+    let arg = env::args().nth(1).expect("Please provide the file to read from as first argument");
+    let path = Path::new(&arg);
+
+    let mut cur = Cursive::new();
+    let explanation = TextView::new("Use the keys ↑ ↓ ← → to navigate around.\nUse q to exit.");
+    let view = HexView::new_from_iter(read_file(path).expect("Cannot read file")).display_state(DisplayState::Enabled);
+
+    cur.add_layer(Dialog::around(LinearLayout::vertical().child(explanation).child(DummyView).child(view)).title("HexView"));
+    cur.add_global_callback('q', |cur| cur.quit());
+    cur.run();
+}
