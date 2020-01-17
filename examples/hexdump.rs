4b783fb0a3ba43eca0c992a3c40fb2545f31fe5d
Alexandre Bury <alexandre.bury@gmail.com>
2020-01-17 15:00:36 -0800

    Update cursive to 0.14

--- examples/hexdump.rs
+++ examples/hexdump.rs
@@ -1,13 +1,13 @@
 extern crate cursive;
 extern crate cursive_hexview;
 
+use cursive::views::{Dialog, DummyView, LinearLayout, TextView};
+use cursive::Cursive;
+use cursive_hexview::{DisplayState, HexView};
 use std::env;
 use std::fs::File;
 use std::io::{self, Read};
 use std::path::Path;
-use cursive_hexview::{DisplayState, HexView};
-use cursive::Cursive;
-use cursive::views::{Dialog, DummyView, LinearLayout, TextView};
 
 fn read_file(path: &Path) -> Result<Vec<u8>, io::Error> {
     let mut file = File::open(path)?;
@@ -17,14 +17,18 @@ fn read_file(path: &Path) -> Result<Vec<u8>, io::Error> {
 }
 
 fn main() {
-    let arg = env::args().nth(1).expect("Please provide the file to read from as first argument");
+    let arg = env::args()
+        .nth(1)
+        .expect("Please provide the file to read from as first argument");
     let path = Path::new(&arg);
 
-    let mut cur = Cursive::ncurses();
+    let mut cur = Cursive::default();
     let explanation = TextView::new("Use the keys ↑ ↓ ← → to navigate around.\nUse q to exit.");
     let view = HexView::new_from_iter(read_file(path).expect("Cannot read file")).display_state(DisplayState::Enabled);
 
-    cur.add_layer(Dialog::around(LinearLayout::vertical().child(explanation).child(DummyView).child(view)).title("HexView"));
+    cur.add_layer(
+        Dialog::around(LinearLayout::vertical().child(explanation).child(DummyView).child(view)).title("HexView"),
+    );
     cur.add_global_callback('q', |cur| cur.quit());
     cur.run();
 }
